b3c04af2e580411fe6b7fb19d54db0faea143447
Arunprasathkumar <Gopinathanraman13790>
2018-11-21 16:21:41 +0530

    Test Case Added

---
+++ JavaTest/src/org/greens/test/Sample2.java
@@ -0,0 +1,14 @@
+package org.greens.test;
+
+public class Sample2 {
+	
+	private void Employee() {
+		// TODO Auto-generated method stub
+ System.out.println("Employee");
+	}
+	public static void main(String[] args) {
+		
+		Sample2 s=new Sample2();
+		s.Employee();
+	}
+}
---
+++ JavaTest/src/org/greens/test/SampleJavaProject.java
@@ -0,0 +1,13 @@
+package org.greens.test;
+
+public class SampleJavaProject {
+	private void JavaProject() {
+		// TODO Auto-generated method stub
+  System.out.println("Sample java project"); 
+	}
+
+	public static void main(String[] args) {
+		SampleJavaProject s=new SampleJavaProject();
+		s.JavaProject();
+	}
+}
